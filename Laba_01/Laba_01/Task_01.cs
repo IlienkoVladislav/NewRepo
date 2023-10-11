a4c53414f4230f95c8bc5dd502ebc3ee5d3f071d
IlienkoVladislav <147603955+IlienkoVladislav@users.noreply.github.com>
2023-10-11 14:42:09 +0300

    Add files via upload

---
+++ Laba_01/Laba_01/Task_01.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+public class Address
+{
+    private int _index;
+    private string _country;
+    private string _city;
+    private string _street;
+    private string _house;
+    private string _apartment;
+
+    public int Index
+    {
+        get { return _index; }
+        set { _index = value; }
+    }
+
+    public string Country
+    {
+        get { return _country; }
+        set { _country = value; }
+    }
+
+    public string City
+    {
+        get { return _city; }
+        set { _city = value; }
+    }
+
+    public string Street
+    {
+        get { return _street; }
+        set { _street = value; }
+    }
+
+    public string House
+    {
+        get { return _house; }
+        set { _house = value; }
+    }
+
+    public string Apartment
+    {
+        get { return _apartment; }
+        set { _apartment = value; }
+    }
+}
+
+class Task_01
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Address address = new Address();
+
+        address.Index = 12345;
+        address.Country = "Україна";
+        address.City = "Київ";
+        address.Street = "Вулиця";
+        address.House = "1";
+        address.Apartment = "2";
+
+        Console.WriteLine($"Індекс: {address.Index}");
+        Console.WriteLine($"Країна: {address.Country}");
+        Console.WriteLine($"Місто: {address.City}");
+        Console.WriteLine($"Вулиця: {address.Street}");
+        Console.WriteLine($"Будинок: {address.House}");
+        Console.WriteLine($"Квартира: {address.Apartment}");
+
+        Console.ReadLine();
+    }
+}
---
+++ Laba_01/Laba_01/Task_02.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Employee
+{
+    private string _lastName;
+    private string _firstName;
+    private string _position;
+
+    public Employee(string lastName, string firstName)
+    {
+        _lastName = lastName;
+        _firstName = firstName;
+    }
+
+    public void SetPosition(string position)
+    {
+        _position = position;
+    }
+
+    public void CalculateSalary(int experience)
+    {
+        double baseSalary = 1000; 
+        double salary = baseSalary + (experience * 100); 
+        double tax = salary * 0.15; 
+
+        Console.WriteLine($"Прізвище: {_lastName}");
+        Console.WriteLine($"Ім'я: {_firstName}");
+        Console.WriteLine($"Посада: {_position}");
+        Console.WriteLine($"Оклад: {salary} USD");
+        Console.WriteLine($"Податковий збір: {tax} USD");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть прізвище співробітника:");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Введіть ім'я співробітника:");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Введіть посаду співробітника:");
+        string position = Console.ReadLine();
+
+        Console.WriteLine("Введіть стаж співробітника (роки):");
+        int experience = Convert.ToInt32(Console.ReadLine());
+
+        Employee employee = new Employee(lastName, firstName);
+        employee.SetPosition(position);
+        employee.CalculateSalary(experience);
+
+        Console.ReadLine();
+    }
+}
